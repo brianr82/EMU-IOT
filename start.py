28440fd16341a911ba7402008d08fa25e24911c8
Brian <brian.ramprasad@gmail.com>
2017-07-28 02:25:47 -0400

    *removed true parameter to docker run for producer

--- start.py
+++ start.py
@@ -192,7 +192,7 @@ def workloadB():
     '''
     #create the producers as needed
 
-    number_of_msg_to_send = 4000
+    number_of_msg_to_send = 10000
     producer_device_delay = 1500000
 
     #Step 1_A - Create sensor pairs in batches of 15 every 3 minutes for 18 minutes (sleep for 60 x 3 = 180 seconds)
