0c37a15b4d5d8838cce6d586080246e0f62ea090
Brian <brian.ramprasad@gmail.com>
2018-03-05 02:56:30 -0500

    Added IOTGateway, Iot Temperature sensor

--- IoTGateway.py
+++ IoTGateway.py
@@ -1,8 +1,8 @@
 from IoTNode import *
 
 class IoTGateway(IoTNode):
-    def __init__(self, gateway_name):
+    def __init__(self, gateway_name, gateway_app_port,max_number_iot_devices_supported):
         assert isinstance(gateway_name, IoTGateway)
         self.gateway_name = gateway_name
-        self.starting_port_range = 3000
-        self.current_port = 3000
+        self.gateway_app_port = gateway_app_port
+        self.max_number_iot_devices_supported = max_number_iot_devices_supported
